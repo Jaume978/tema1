d7510cedab09f53acc34c587e043000f8edfba54
jamifo@alumnatflorida <jamifo@alumnatflorida.es>
2024-11-25 20:31:15 +0100

    Añado función saludarPoNombre en main

--- src/functions.php
+++ src/functions.php
@@ -1,2 +1,3 @@
 <?php function saludar() { return "Hola desde la función!"; } ?>
 <?php function saludarPorNombre($nombre) { return "Hola, $nombre!"; } ?>
+<?php function saludarPorNombre($nombre) { return "Hola, $nombre!"; } ?>
